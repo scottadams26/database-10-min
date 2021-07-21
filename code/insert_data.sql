18b2ed3b12bdaf66b97e7f8e86248441e188a516
Scott Adams <scottadams26@gmail.com>
2021-07-20 19:30:09 -0500

    Added student_courses INSERT and data for 1003

--- code/insert_data.sql
+++ code/insert_data.sql
@@ -1,7 +1,12 @@
+INSERT INTO student_courses (student_id, course_id, grade)
+VALUES
+        ("1003", "BIO355", "A"),
+        ("1003", "BIO375", "A")
+;
+
 INSERT INTO students (student_id, student_first_name, student_last_name)
 VALUES
-        ("1001", "Jane", "Lane"),
-        ("1002", "John", "Smith")
+        ("1003", "John", "Snow")
 ;
 
 
